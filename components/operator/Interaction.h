876b5269dcd63042883b884d6b269d2925da619e
jrojer <cibid@yandex.ru>
2021-10-20 08:39:15 +0300

    add virtual destructors

--- components/operator/Interaction.h
+++ components/operator/Interaction.h
@@ -12,18 +12,23 @@ class ZeroCenterTerm {
 public:
     virtual void construct(LexicograficalMatrix& matrix_in_lexicografical_basis, const spaces::LexicographicIndexConverter& converter,
                            uint32_t index_of_vector) const = 0;
+    virtual ~ZeroCenterTerm(){};
 };
 
 class OneCenterTerm {
 public:
     virtual void construct(LexicograficalMatrix& matrix_in_lexicografical_basis, const spaces::LexicographicIndexConverter& converter,
                            uint32_t index_of_vector, uint32_t center_a) const = 0;
+    virtual ~OneCenterTerm(){};
+
 };
 
 class TwoCenterTerm {
 public:
     virtual void construct(LexicograficalMatrix& matrix_in_lexicografical_basis, const spaces::LexicographicIndexConverter& converter,
                            uint32_t index_of_vector, uint32_t center_a, uint32_t center_b) const = 0;
+    virtual ~TwoCenterTerm(){};
+
 };
 
 #endif //JULY_INTERACTION_H
