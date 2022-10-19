3e03ab56dfc01d870ddf4c3db53cec3ab46212b5
Eugen Kadienko <emkad@yandex.ru>
2022-10-19 20:15:14 +0700

    Create AbstractNonlinearSolver for different solver packages.

---
+++ src/nonlinear_solver/AbstractNonlinearSolver.h
@@ -0,0 +1,18 @@
+#ifndef SPINNER_ABSTRACTNONLINEARSOLVER_H
+#define SPINNER_ABSTRACTNONLINEARSOLVER_H
+
+#include <functional>
+#include <vector>
+
+namespace nonlinear_solver {
+
+class AbstractNonlinearSolver {
+  public:
+    virtual void optimize(
+        std::function<double(const std::vector<double>&, std::vector<double>&)> oneStepFunction,
+        std::vector<double>& changeable_values) = 0;
+};
+
+}  // namespace nonlinear_solver
+
+#endif  //SPINNER_ABSTRACTNONLINEARSOLVER_H
