cc60fbb4cd94b22b6114e789127b33421e6e4e33
Eugen Kadienko <emkad@yandex.ru>
2022-10-27 14:28:25 +0700

    Add GSzSquaredWorker.

---
+++ src/entities/magnetic_susceptibility/worker/GSzSquaredWorker.h
@@ -0,0 +1,26 @@
+#ifndef SPINNER_GSZSQUAREDWORKER_H
+#define SPINNER_GSZSQUAREDWORKER_H
+
+#include "BasicWorker.h"
+
+namespace magnetic_susceptibility::worker {
+
+// mu^2 = mu_B^2 * <(\sum_i g_i S_{iz})^2>
+class GSzSquaredWorker: public BasicWorker {
+  public:
+    GSzSquaredWorker(
+        std::unique_ptr<quantum::linear_algebra::AbstractVector>&& energy,
+        std::unique_ptr<quantum::linear_algebra::AbstractVector>&& degeneracy,
+        std::unique_ptr<quantum::linear_algebra::AbstractVector>&& g_sz_squared);
+
+    double calculateTheoreticalMuSquared(double temperature) const override;
+    std::vector<ValueAtTemperature> calculateDerivative(
+        model::symbols::SymbolTypeEnum symbol_type,
+        std::map<common::QuantityEnum, std::unique_ptr<quantum::linear_algebra::AbstractVector>>
+            values_derivatives_map) const override;
+
+  private:
+    std::unique_ptr<quantum::linear_algebra::AbstractVector> g_sz_squared_;
+};
+}  // namespace magnetic_susceptibility::worker
+#endif  //SPINNER_GSZSQUAREDWORKER_H
