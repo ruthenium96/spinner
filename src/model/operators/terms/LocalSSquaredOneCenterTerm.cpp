c95f2043707abff6d24648214358e383e1fd35b4
Eugen Kadienko <emkad@yandex.ru>
2022-11-14 18:31:03 +0700

    Add LocalSSquaredOneCenterTerm.

---
+++ src/model/operators/terms/LocalSSquaredOneCenterTerm.cpp
@@ -0,0 +1,40 @@
+#include "LocalSSquaredOneCenterTerm.h"
+
+#include <cmath>
+#include <utility>
+
+namespace model::operators {
+
+std::unique_ptr<OneCenterTerm> LocalSSquaredOneCenterTerm::clone() const {
+    return std::make_unique<LocalSSquaredOneCenterTerm>(converter_, coefficients_, prefactor_);
+}
+void LocalSSquaredOneCenterTerm::construct(
+    UnitarySparseMatrix& matrix_in_lexicografical_basis,
+    uint32_t index_of_vector,
+    uint32_t center_a) const {
+    double factor = coefficients_->at(center_a) * prefactor_;
+
+    if (!std::isnan(factor)) {
+        double spin = converter_.get_spins()[center_a];
+
+        double diagonal_value = factor * spin * (spin + 1);
+        matrix_in_lexicografical_basis.add_to_position(
+            diagonal_value,
+            index_of_vector,
+            index_of_vector);
+    }
+}
+
+LocalSSquaredOneCenterTerm::LocalSSquaredOneCenterTerm(
+    lexicographic::IndexConverter converter,
+    std::shared_ptr<const OneDNumericalParameters<double>> coefficients,
+    double prefactor) :
+    converter_(std::move(converter)),
+    coefficients_(std::move(coefficients)),
+    prefactor_(prefactor) {}
+LocalSSquaredOneCenterTerm::LocalSSquaredOneCenterTerm(
+    lexicographic::IndexConverter converter,
+    std::shared_ptr<const OneDNumericalParameters<double>> coefficients) :
+    converter_(std::move(converter)),
+    coefficients_(std::move(coefficients)) {}
+}  // namespace model::operators
---
+++ src/model/operators/terms/LocalSSquaredOneCenterTerm.h
@@ -0,0 +1,30 @@
+#ifndef SPINNER_LOCALSSQUAREDONECENTERTERM_H
+#define SPINNER_LOCALSSQUAREDONECENTERTERM_H
+
+#include "Term.h"
+
+namespace model::operators {
+class LocalSSquaredOneCenterTerm: public OneCenterTerm {
+  public:
+    LocalSSquaredOneCenterTerm(
+        lexicographic::IndexConverter converter,
+        std::shared_ptr<const OneDNumericalParameters<double>> coefficients);
+    LocalSSquaredOneCenterTerm(
+        lexicographic::IndexConverter converter,
+        std::shared_ptr<const OneDNumericalParameters<double>> coefficients,
+        double prefactor);
+    std::unique_ptr<OneCenterTerm> clone() const override;
+    void construct(
+        UnitarySparseMatrix& matrix_in_lexicografical_basis,
+        uint32_t index_of_vector,
+        uint32_t center_a) const override;
+
+  private:
+    const lexicographic::IndexConverter converter_;
+    std::shared_ptr<const OneDNumericalParameters<double>> coefficients_;
+    const double prefactor_ = 1;
+};
+
+}  // namespace model::operators
+
+#endif  //SPINNER_LOCALSSQUAREDONECENTERTERM_H
