0864364a7ad56218959e4a0551b7035e12239ddf
Eugen Kadienko <emkad@yandex.ru>
2022-11-24 20:00:24 +0700

    Fix renaming of DenseClasses

--- src/entities/spectrum/Subspectrum.cpp
+++ src/entities/spectrum/Subspectrum.cpp
@@ -9,14 +9,14 @@ std::ostream& operator<<(std::ostream& os, const Subspectrum& subspectrum) {
     return os;
 }
 
-std::pair<Subspectrum, std::unique_ptr<quantum::linear_algebra::AbstractMatrix>>
+std::pair<Subspectrum, std::unique_ptr<quantum::linear_algebra::AbstractDenseMatrix>>
 Subspectrum::energy(const Submatrix& hamiltonian_submatrix) {
     auto eigencouple = hamiltonian_submatrix.raw_data->diagonalizeValuesVectors();
 
     auto energy_subspectrum =
         Subspectrum(std::move(eigencouple.eigenvalues), hamiltonian_submatrix.properties);
 
-    std::pair<Subspectrum, std::unique_ptr<quantum::linear_algebra::AbstractMatrix>> answer = {
+    std::pair<Subspectrum, std::unique_ptr<quantum::linear_algebra::AbstractDenseMatrix>> answer = {
         std::move(energy_subspectrum),
         std::move(eigencouple.eigenvectors)};
     return answer;
@@ -24,7 +24,8 @@ Subspectrum::energy(const Submatrix& hamiltonian_submatrix) {
 
 Subspectrum Subspectrum::non_energy(
     const Submatrix& non_hamiltonian_submatrix,
-    const std::unique_ptr<quantum::linear_algebra::AbstractMatrix>& unitary_transformation_matrix) {
+    const std::unique_ptr<quantum::linear_algebra::AbstractDenseMatrix>&
+        unitary_transformation_matrix) {
     // TODO: I guess, we can do it faster. If B = U * A * U^T,
     //  we need only B_{ii} = \sum_{k} \sum_{l} U_{ik}*U_{il}*A_{kl}
     //  We can multiply A * U^T directly for O(N^3), then calculate all B_{ii} for O(N^2).
@@ -38,7 +39,7 @@ Subspectrum Subspectrum::non_energy(
     return non_energy_subspectrum;
 }
 Subspectrum::Subspectrum(
-    std::unique_ptr<quantum::linear_algebra::AbstractVector> raw_data_,
+    std::unique_ptr<quantum::linear_algebra::AbstractDenseVector> raw_data_,
     BlockProperties properties_) {
     raw_data = std::move(raw_data_);
     properties = std::move(properties_);
--- src/entities/spectrum/Subspectrum.h
+++ src/entities/spectrum/Subspectrum.h
@@ -2,24 +2,24 @@
 #define SPINNER_SUBSPECTRUM_H
 
 #include "src/entities/BlockProperties.h"
-#include "src/entities/data_structures/AbstractMatrix.h"
-#include "src/entities/data_structures/AbstractVector.h"
+#include "src/entities/data_structures/AbstractDenseMatrix.h"
+#include "src/entities/data_structures/AbstractDenseVector.h"
 #include "src/entities/matrix/Submatrix.h"
 
 struct Subspectrum {
     BlockProperties properties;
-    std::unique_ptr<quantum::linear_algebra::AbstractVector> raw_data;
+    std::unique_ptr<quantum::linear_algebra::AbstractDenseVector> raw_data;
 
     Subspectrum() = delete;
     Subspectrum(
-        std::unique_ptr<quantum::linear_algebra::AbstractVector> raw_data_,
+        std::unique_ptr<quantum::linear_algebra::AbstractDenseVector> raw_data_,
         BlockProperties properties_);
 
-    static std::pair<Subspectrum, std::unique_ptr<quantum::linear_algebra::AbstractMatrix>>
+    static std::pair<Subspectrum, std::unique_ptr<quantum::linear_algebra::AbstractDenseMatrix>>
     energy(const Submatrix& hamiltonian_submatrix);
     static Subspectrum non_energy(
         const Submatrix& non_hamiltonian_submatrix,
-        const std::unique_ptr<quantum::linear_algebra::AbstractMatrix>&
+        const std::unique_ptr<quantum::linear_algebra::AbstractDenseMatrix>&
             unitary_transformation_matrix);
 
     friend std::ostream& operator<<(std::ostream& os, const Subspectrum& subspectrum);
