6b0a68b23ea4bc99c94fd544af6a3e5e1480e768
Eugen Kadienko <emkad@yandex.ru>
2023-07-25 17:27:31 +0700

    Create S2Transformer class.

---
+++ src/space/optimization/S2Transformer.cpp
@@ -0,0 +1,124 @@
+#include "S2Transformer.h"
+
+#include <cassert>
+#include <utility>
+
+#include "wignerSymbols.h"
+
+namespace space::optimization {
+
+S2Transformer::S2Transformer(
+    lexicographic::IndexConverter converter,
+    quantum::linear_algebra::FactoriesList factories) :
+    converter_(std::move(converter)),
+    factories_(std::move(factories)) {
+    {  // TODO: it is construction of chain-order summation. Move it to another place.
+        order_of_summation_ = std::make_shared<std::vector<spin_algebra::AdditionInstruction>>();
+
+        auto number_of_mults = converter_.get_mults().size();
+
+        for (size_t i = 0; i < number_of_mults - 1; ++i) {
+            order_of_summation_->push_back({i, number_of_mults + i - 1});
+        }
+    }
+
+    sorted_s_squared_states_ = spin_algebra::SSquaredState::addAllMultiplicitiesAndSort(
+        converter_.get_mults(),
+        order_of_summation_);
+}
+
+space::Space S2Transformer::apply(Space&& space) const {
+    std::vector<Subspace> vector_result;
+    vector_result.reserve(space.getBlocks().size());
+
+    for (size_t i = 0; i < space.getBlocks().size(); ++i) {
+        Subspace& subspace = space.getBlocks()[i];
+
+        // It is total_proj + max_total_spin = ntz_value
+        // We need 2 * |total_proj| + 1 = 2 * |ntz_value - max_total_spin| + 1 =
+        // |2 * ntz_value - 2 * max_total_spin - 1 + 1| + 1 =
+        // |2 * ntz_value - max_total_proj + 1| + 1
+        auto ntz_value = subspace.properties.n_proj.value();
+        int a = 2 * (int)ntz_value + 1 - (int)converter_.get_max_ntz_proj();
+        spin_algebra::Multiplicity current_mult = std::abs(a) + 1;
+
+        const auto& history = sorted_s_squared_states_.at(current_mult);
+
+        subspace.dense_semiunitary_matrix = constructTransformationMatrix(history, subspace);
+
+        subspace.properties.total_mult = current_mult;
+        subspace.properties.degeneracy *= current_mult;
+        // TODO: for cases with TzSorter and without PositiveProjectionsEliminator:
+        if (current_mult != 1) {
+            subspace.properties.degeneracy /= 2;
+        }
+
+        vector_result.emplace_back(std::move(subspace));
+    }
+
+    return space::Space(std::move(vector_result));
+}
+
+std::unique_ptr<quantum::linear_algebra::AbstractDenseSemiunitaryMatrix>
+S2Transformer::constructTransformationMatrix(
+    const std::vector<spin_algebra::SSquaredState>& history,
+    const Subspace& subspace) const {
+    auto number_of_sz_states = subspace.decomposition->size_cols();
+    auto number_of_s2_states = history.size();
+    auto transformation_matrix =
+        factories_.createDenseSemiunitaryMatrix(number_of_s2_states, number_of_sz_states);
+
+    for (size_t i = 0; i < number_of_s2_states; ++i) {
+        const auto& ololo = history[i];
+        for (size_t j = 0; j < number_of_sz_states; ++j) {
+            auto lex_index = subspace.decomposition->GetNewIterator(j)->getNext().index;
+
+            double value = total_CG_coefficient(ololo, lex_index);
+            transformation_matrix->add_to_position(value, i, j);
+        }
+    }
+    return transformation_matrix;
+}
+
+double S2Transformer::total_CG_coefficient(
+    const spin_algebra::SSquaredState& history,
+    uint32_t lex_index) const {
+    auto number_of_mults = converter_.get_mults().size();
+
+    double c = 1;
+
+    // todo: check if it is correct:
+    std::vector<double> projections;
+    projections.resize(2 * number_of_mults - 1);
+    for (size_t i = 0; i < number_of_mults; ++i) {
+        projections[i] = converter_.convert_lex_index_to_one_sz_projection(lex_index, i)
+            - converter_.get_spins()[i];
+    }
+    for (size_t i = 0; i < number_of_mults - 1; ++i) {
+        projections[i + number_of_mults] = projections[i] + projections[i + number_of_mults - 1];
+    }
+
+    for (size_t i = 0; i < order_of_summation_->size(); ++i) {
+        size_t pos_one = order_of_summation_->at(i)[0];
+        size_t pos_two = order_of_summation_->at(i)[1];
+        size_t pos_sum = number_of_mults + i;  // chain-like summation
+
+        double spin_one = ((double)history.getMultiplicity(pos_one) - 1.0) / 2.0;
+        double spin_two = ((double)history.getMultiplicity(pos_two) - 1.0) / 2.0;
+        double spin_sum = ((double)history.getMultiplicity(pos_sum) - 1.0) / 2.0;
+
+        double proj_one = projections[pos_one];
+        double proj_two = projections[pos_two];
+        c *= hashed_clebsh_gordan(spin_one, spin_two, spin_sum, proj_one, proj_two);
+        if (c == 0.0) {
+            return c;
+        }
+    }
+
+    return c;
+}
+
+double S2Transformer::hashed_clebsh_gordan(double l1, double l2, double l3, double m1, double m2) {
+    return WignerSymbols::clebschGordan(l1, l2, l3, m1, m2, m1 + m2);
+}
+}  // namespace space::optimization
---
+++ src/space/optimization/S2Transformer.h
@@ -0,0 +1,39 @@
+#ifndef SPINNER_S2TRANSFORMER_H
+#define SPINNER_S2TRANSFORMER_H
+
+#include <numeric>
+#include <utility>
+
+#include "src/common/lexicographic/IndexConverter.h"
+#include "src/entities/matrix/Matrix.h"
+#include "src/space/Space.h"
+#include "src/spin_algebra/SSquaredState.h"
+
+namespace space::optimization {
+class S2Transformer {
+  public:
+    S2Transformer(
+        lexicographic::IndexConverter converter,
+        quantum::linear_algebra::FactoriesList factories);
+
+    space::Space apply(space::Space&& space) const;
+
+  private:
+    // TODO: move these functions to another class?
+    std::unique_ptr<quantum::linear_algebra::AbstractDenseSemiunitaryMatrix>
+    constructTransformationMatrix(
+        const std::vector<spin_algebra::SSquaredState>& history,
+        const space::Subspace& subspace) const;
+    double
+    total_CG_coefficient(const spin_algebra::SSquaredState& history, uint32_t lex_index) const;
+    static double hashed_clebsh_gordan(double l1, double l2, double l3, double m1, double m2);
+
+    const lexicographic::IndexConverter converter_;
+    const quantum::linear_algebra::FactoriesList factories_;
+
+    std::shared_ptr<std::vector<spin_algebra::AdditionInstruction>> order_of_summation_;
+    std::map<spin_algebra::Multiplicity, std::vector<spin_algebra::SSquaredState>>
+        sorted_s_squared_states_;
+};
+}  // namespace space::optimization
+#endif  //SPINNER_S2TRANSFORMER_H
