91c59db7a5ef7a49a5f1373dabb311e2c5a3984e
Eugen Kadienko <emkad@yandex.ru>
2023-07-25 15:21:08 +0700

    Add total_mult to BlockProperties.

--- src/entities/BlockProperties.h
+++ src/entities/BlockProperties.h
@@ -10,8 +10,10 @@ struct BlockProperties {
     std::string get_representation_name() const;
 
     std::optional<uint32_t> n_proj = std::nullopt;
+    std::optional<uint32_t> total_mult = std::nullopt;
     uint32_t dimensionality = 1;
-    uint32_t degeneracy = 1;
+    // TODO: double instead int for cases with TzSorter and without PositiveProjectionsEliminator:
+    double degeneracy = 1;
     std::vector<uint32_t> representation;
 
     bool operator==(const BlockProperties&) const = default;
