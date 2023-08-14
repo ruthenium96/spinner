9984d2b8ac70ae186cc5f3ab81fc4b6b7e497367
Eugen Kadienko <emkad@yandex.ru>
2023-08-14 19:24:38 +0700

    Add representations to SSquaredStates.

--- src/spin_algebra/SSquaredState.cpp
+++ src/spin_algebra/SSquaredState.cpp
@@ -9,9 +9,13 @@ namespace spin_algebra {
 
 SSquaredState::SSquaredState(
     std::shared_ptr<std::vector<Multiplicity>> initialMultiplicities,
-    size_t number_of_summations) {
+    size_t number_of_summations,
+    size_t number_of_representations) {
     initialMultiplicities_ = std::move(initialMultiplicities);
     intermediateMultiplicities_.resize(number_of_summations, 0);
+    intermediateRepresentations_ = std::vector<std::vector<std::optional<uint8_t>>>(
+        getSize(),
+        std::vector<std::optional<uint8_t>>(number_of_representations, std::nullopt));
 }
 
 Multiplicity SSquaredState::getMultiplicity(size_t number) const {
@@ -41,20 +45,25 @@ size_t SSquaredState::getSize() const {
     return initialMultiplicities_->size() + intermediateMultiplicities_.size();
 }
 
-Multiplicity SSquaredState::back() const {
-    if (!intermediateMultiplicities_.empty()) {
-        return intermediateMultiplicities_.back();
-    } else {
-        return initialMultiplicities_->back();
+SSquaredState::Properties SSquaredState::back() const {
+    Properties properties;
+    properties.multiplicity = intermediateMultiplicities_.back();
+    for (const auto& mb_representaton : intermediateRepresentations_.back()) {
+        properties.representations.push_back(mb_representaton.value());
     }
+    return properties;
 }
 
-std::map<Multiplicity, std::vector<SSquaredState>> SSquaredState::addAllMultiplicitiesAndSort(
+std::map<SSquaredState::Properties, std::vector<SSquaredState>>
+SSquaredState::addAllMultiplicitiesAndSort(
     const std::vector<Multiplicity>& multiplicities_to_sum,
-    const std::shared_ptr<const OrderOfSummation>& order_of_summation) {
+    const std::shared_ptr<const OrderOfSummation>& order_of_summation,
+    const std::vector<std::map<std::pair<uint8_t, uint8_t>, std::set<uint8_t>>>&
+        all_groups_multiplication_tables) {
     auto empty_history = SSquaredState(
         std::make_shared<std::vector<Multiplicity>>(multiplicities_to_sum),
-        order_of_summation->size());
+        order_of_summation->size(),
+        all_groups_multiplication_tables.size());
 
     std::vector<SSquaredState> result_of_summation = {empty_history};
 
@@ -70,29 +79,99 @@ std::map<Multiplicity, std::vector<SSquaredState>> SSquaredState::addAllMultipli
             MultiplicityDirectSum mult_two(history.getMultiplicity(pos_two));
             auto mult_direct_sum = mult_one * mult_two;
             for (auto mult_sum : mult_direct_sum.getMultiplicities()) {
+                auto representations_one = history.getRepresentations(pos_one);
+                auto representations_two = history.getRepresentations(pos_two);
+                auto representations_sum = multiplyRepresentations(
+                    representations_one,
+                    representations_two,
+                    instruction,
+                    all_groups_multiplication_tables,
+                    mult_one.getMultiplicities().at(0),
+                    mult_two.getMultiplicities().at(0),
+                    mult_sum);
                 temp_result.push_back(history);
                 temp_result.back().setMultiplicity(pos_sum, mult_sum);
+                temp_result.back().setRepresentations(pos_sum, representations_sum);
             }
         }
         std::swap(result_of_summation, temp_result);
         temp_result.clear();
     }
 
-    auto result_of_sum_and_sort = std::map<Multiplicity, std::vector<SSquaredState>>();
+    auto result_of_sum_and_sort = std::map<Properties, std::vector<SSquaredState>>();
 
     for (auto history : result_of_summation) {
-        // TODO: implement .final() instead of .back()
-        //  currently, position_of_sum of the last addition should point to
-        //  the last element of intermediateMultiplicities_
-        auto final_multiplicity = history.back();
+        auto final_properties = history.back();
 
-        if (result_of_sum_and_sort.count(final_multiplicity) == 0) {
-            result_of_sum_and_sort[final_multiplicity] = std::vector<SSquaredState>();
+        if (result_of_sum_and_sort.count(final_properties) == 0) {
+            result_of_sum_and_sort[final_properties] = std::vector<SSquaredState>();
         }
-        result_of_sum_and_sort[final_multiplicity].emplace_back(std::move(history));
+        result_of_sum_and_sort[final_properties].emplace_back(std::move(history));
     }
 
     return result_of_sum_and_sort;
+}
+
+const std::vector<std::optional<uint8_t>>& SSquaredState::getRepresentations(size_t number) const {
+    return intermediateRepresentations_.at(number);
+}
+
+void SSquaredState::setRepresentations(
+    size_t number,
+    std::vector<std::optional<uint8_t>> representation) {
+    intermediateRepresentations_[number] = std::move(representation);
+}
+
+std::vector<std::optional<uint8_t>> SSquaredState::multiplyRepresentations(
+    const std::vector<std::optional<uint8_t>>& representations_one,
+    const std::vector<std::optional<uint8_t>>& representations_two,
+    const OrderOfSummation::AdditionInstruction& instruction,
+    const std::vector<std::map<std::pair<uint8_t, uint8_t>, std::set<uint8_t>>>&
+        all_groups_multiplication_tables,
+    Multiplicity mult_one,
+    Multiplicity mult_two,
+    Multiplicity mult_sum) {
+    auto representations_sum = std::vector<std::optional<uint8_t>>(representations_one.size());
+    for (size_t number_of_group = 0; number_of_group < representations_one.size();
+         ++number_of_group) {
+        if (instruction.number_of_group != number_of_group) {
+            // outside orbit summation
+            const auto& multiplication_table = all_groups_multiplication_tables[number_of_group];
+            if (representations_one[number_of_group].has_value()
+                && representations_two[number_of_group].has_value()) {
+                auto result_representation = multiplication_table.at(
+                    {representations_one[number_of_group].value(),
+                     representations_two[number_of_group].value()});
+                // TODO: the first (and currently the only) representation
+                representations_sum[number_of_group] = *result_representation.begin();
+            } else if (representations_one[number_of_group].has_value()) {
+                representations_sum[number_of_group] = representations_one[number_of_group].value();
+            } else if (representations_two[number_of_group].has_value()) {
+                representations_sum[number_of_group] = representations_two[number_of_group].value();
+            } else {
+                representations_sum[number_of_group] = 0;
+            }
+        } else {
+            // inside orbit summation
+            // TODO: It is correct only for P2 group, move it from here:
+            if (mult_one == mult_two) {
+                // M1 = 2S1 + 1
+                // M2 = 2S2 + 1
+                // Mm = 2(S1+S2) + 1 = (2S1+1) + (2S2+1) - 1 = M1 + M2 - 1
+                Multiplicity maxMultiplicity = mult_one + mult_two - 1;
+                if ((maxMultiplicity - mult_sum) % 4 == 0) {
+                    representations_sum[number_of_group] = 0;
+                } else {
+                    representations_sum[number_of_group] = 1;
+                }
+            } else if (mult_one > mult_two) {
+                representations_sum[number_of_group] = 0;
+            } else if (mult_one < mult_two) {
+                representations_sum[number_of_group] = 1;
+            }
+        }
+    }
+    return representations_sum;
 }
 
 }  // namespace spin_algebra
--- src/spin_algebra/SSquaredState.h
+++ src/spin_algebra/SSquaredState.h
@@ -14,19 +14,41 @@ class SSquaredState {
   private:
     std::shared_ptr<std::vector<Multiplicity>> initialMultiplicities_;
     std::vector<Multiplicity> intermediateMultiplicities_;
-    // TODO: std::vector of representations?
+    // first vector over different centers, second -- over different groups
+    std::vector<std::vector<std::optional<uint8_t>>> intermediateRepresentations_;
     SSquaredState(
         std::shared_ptr<std::vector<Multiplicity>> initialMultiplicities,
-        size_t number_of_summations);
+        size_t number_of_summations,
+        size_t number_of_representations = 0);
     void setMultiplicity(size_t number, Multiplicity multiplicity);
+    void setRepresentations(size_t number, std::vector<std::optional<uint8_t>> representation);
+    // TODO: move it from this class:
+    static std::vector<std::optional<uint8_t>> multiplyRepresentations(
+        const std::vector<std::optional<uint8_t>>& representation_one,
+        const std::vector<std::optional<uint8_t>>& representation_two,
+        const OrderOfSummation::AdditionInstruction& instruction,
+        const std::vector<std::map<std::pair<uint8_t, uint8_t>, std::set<uint8_t>>>&
+            all_groups_multiplication_tables,
+        Multiplicity mult_one,
+        Multiplicity mult_two,
+        Multiplicity mult_sum);
 
   public:
+    struct Properties {
+        Multiplicity multiplicity;
+        std::vector<uint8_t> representations;
+        std::strong_ordering operator<=>(const Properties& rhs) const = default;
+    };
+
     Multiplicity getMultiplicity(size_t number) const;
+    const std::vector<std::optional<uint8_t>>& getRepresentations(size_t number) const;
     size_t getSize() const;
-    Multiplicity back() const;
-    static std::map<Multiplicity, std::vector<SSquaredState>> addAllMultiplicitiesAndSort(
+    Properties back() const;
+    static std::map<Properties, std::vector<SSquaredState>> addAllMultiplicitiesAndSort(
         const std::vector<Multiplicity>& multiplicities_to_sum,
-        const std::shared_ptr<const OrderOfSummation>& order_of_summation);
+        const std::shared_ptr<const OrderOfSummation>& order_of_summation,
+        const std::vector<std::map<std::pair<uint8_t, uint8_t>, std::set<uint8_t>>>&
+            all_groups_multiplication_tables);
 };
 
 }  // namespace spin_algebra
