7d0a11b7dd55ff63669ece95e219bfffca34a0c6
Eugen Kadienko <emkad@yandex.ru>
2023-08-17 15:48:35 +0700

    Add Elimination of Positive Projections in performance test.

--- tests/time_tests/PerformanceTest.cpp
+++ tests/time_tests/PerformanceTest.cpp
@@ -28,6 +28,7 @@ TEST(performanceTest, simple2ComponentSchema) {
                 model::ModelInput model(mults);
                 common::physical_optimization::OptimizationList optimizationList;
                 optimizationList.TzSort()
+                    .EliminatePositiveProjections()
                     .Symmetrize(
                         group::Group::S3,
                         {{1, 2, 0, 4, 5, 3, 7, 8, 6}, {0, 2, 1, 3, 5, 4, 6, 8, 7}})
