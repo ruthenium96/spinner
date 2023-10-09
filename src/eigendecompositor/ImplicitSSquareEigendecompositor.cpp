57030679546c8a896009c7e8f85fe09869bf9f74
Eugen Kadienko <emkad@yandex.ru>
2023-10-09 14:16:26 +0700

    Refactor ImplicitSSquareEigendecompositor.

--- src/eigendecompositor/ImplicitSSquareEigendecompositor.cpp
+++ src/eigendecompositor/ImplicitSSquareEigendecompositor.cpp
@@ -1,5 +1,6 @@
 #include "ImplicitSSquareEigendecompositor.h"
 
+#include <cassert>
 #include <utility>
 
 namespace eigendecompositor {
@@ -8,47 +9,53 @@ ImplicitSSquareEigendecompositor::ImplicitSSquareEigendecompositor(
     std::unique_ptr<AbstractEigendecompositor> eigendecompositor,
     quantum::linear_algebra::FactoriesList factories_list) :
     eigendecompositor_(std::move(eigendecompositor)),
-    factories_list_(std::move(factories_list)) {}
+    factories_list_(std::move(factories_list)) {
+    s_square_implicit_ = common::Quantity();
+}
 
-void ImplicitSSquareEigendecompositor::BuildSpectra(
-    const std::map<common::QuantityEnum, std::shared_ptr<const model::operators::Operator>>&
-        operators_,
-    const std::map<
+std::optional<std::shared_ptr<quantum::linear_algebra::AbstractDenseSemiunitaryMatrix>>
+ImplicitSSquareEigendecompositor::BuildSubspectra(
+    std::map<common::QuantityEnum, std::shared_ptr<const model::operators::Operator>>& operators_,
+    std::map<
         std::pair<common::QuantityEnum, model::symbols::SymbolName>,
         std::shared_ptr<const model::operators::Operator>>& derivatives_operators_,
-    const space::Space& space) {
+    size_t number_of_block,
+    const space::Subspace& subspace) {
     if (operators_.contains(common::S_total_squared)) {
         throw std::invalid_argument(
             "Explicit S2 operator passed to ImplicitSSquareEigendecompositor");
     }
 
-    eigendecompositor_->BuildSpectra(operators_, derivatives_operators_, space);
+    auto mb_unitary_transformation_matrix = eigendecompositor_->BuildSubspectra(
+        operators_,
+        derivatives_operators_,
+        number_of_block,
+        subspace);
 
-    if (!s_square_implicit_.has_value()) {
-        const auto& energy_spectrum = eigendecompositor_->getSpectrum(common::Energy)->get();
+    if (!first_iteration_has_been_done_) {
+        const auto& energy_subspectrum =
+            eigendecompositor_->getSpectrum(common::Energy)->get().blocks.at(number_of_block);
 
-        s_square_implicit_ = common::Quantity();
-        s_square_implicit_.value().spectrum_.blocks.reserve(energy_spectrum.blocks.size());
+        double mult = energy_subspectrum.properties.total_mult.value();
+        double spin = (mult - 1) / 2.0;
+        double s_squared_value = spin * (spin + 1);
+        size_t size_of_subspace = energy_subspectrum.raw_data->size();
 
-        for (const auto& subspectrum_energy : energy_spectrum.blocks) {
-            double mult = subspectrum_energy.properties.total_mult.value();
-            double spin = (mult - 1) / 2.0;
-            double s_squared_value = spin * (spin + 1);
-            size_t size = subspectrum_energy.raw_data->size();
-
-            auto raw_data = factories_list_.createVector();
-            raw_data->add_identical_values(size, s_squared_value);
-            s_square_implicit_.value().spectrum_.blocks.emplace_back(
-                std::move(raw_data),
-                subspectrum_energy.properties);
-        }
+        auto raw_data = factories_list_.createVector();
+        raw_data->add_identical_values(size_of_subspace, s_squared_value);
+        s_square_implicit_.spectrum_.blocks.emplace_back(
+            std::move(raw_data),
+            energy_subspectrum.properties);
+        assert(s_square_implicit_.spectrum_.blocks.size() == number_of_block + 1);
     }
+
+    return mb_unitary_transformation_matrix;
 }
 
 std::optional<std::reference_wrapper<const Spectrum>>
 ImplicitSSquareEigendecompositor::getSpectrum(common::QuantityEnum quantity_enum) const {
     if (quantity_enum == common::S_total_squared) {
-        return s_square_implicit_.value().spectrum_;
+        return s_square_implicit_.spectrum_;
     }
     return eigendecompositor_->getSpectrum(quantity_enum);
 }
@@ -80,4 +87,14 @@ ImplicitSSquareEigendecompositor::getMatrixDerivative(
     }
     return eigendecompositor_->getMatrixDerivative(quantity_enum, symbol_name);
 }
+
+void ImplicitSSquareEigendecompositor::initialize() {
+    eigendecompositor_->initialize();
+}
+
+void ImplicitSSquareEigendecompositor::finalize() {
+    eigendecompositor_->finalize();
+    first_iteration_has_been_done_ = true;
+}
+
 }  // namespace eigendecompositor
--- src/eigendecompositor/ImplicitSSquareEigendecompositor.h
+++ src/eigendecompositor/ImplicitSSquareEigendecompositor.h
@@ -5,18 +5,21 @@
 
 namespace eigendecompositor {
 
+// This class implicitly calculate S^2 values using block_properties data (total_mult).
 class ImplicitSSquareEigendecompositor: public AbstractEigendecompositor {
   public:
     ImplicitSSquareEigendecompositor(
         std::unique_ptr<AbstractEigendecompositor> eigendecompositor,
         quantum::linear_algebra::FactoriesList factories_list);
-    void BuildSpectra(
-        const std::map<common::QuantityEnum, std::shared_ptr<const model::operators::Operator>>&
+    std::optional<std::shared_ptr<quantum::linear_algebra::AbstractDenseSemiunitaryMatrix>>
+    BuildSubspectra(
+        std::map<common::QuantityEnum, std::shared_ptr<const model::operators::Operator>>&
             operators_,
-        const std::map<
+        std::map<
             std::pair<common::QuantityEnum, model::symbols::SymbolName>,
             std::shared_ptr<const model::operators::Operator>>& derivatives_operators_,
-        const space::Space& space) override;
+        size_t number_of_block,
+        const space::Subspace& subspace) override;
 
     std::optional<std::reference_wrapper<const Spectrum>>
     getSpectrum(common::QuantityEnum anEnum) const override;
@@ -28,11 +31,14 @@ class ImplicitSSquareEigendecompositor: public AbstractEigendecompositor {
     std::optional<std::reference_wrapper<const Matrix>> getMatrixDerivative(
         common::QuantityEnum anEnum,
         const model::symbols::SymbolName& name) const override;
+    void initialize() override;
+    void finalize() override;
 
   private:
     std::unique_ptr<AbstractEigendecompositor> eigendecompositor_;
-    std::optional<common::Quantity> s_square_implicit_;
+    common::Quantity s_square_implicit_;
     quantum::linear_algebra::FactoriesList factories_list_;
+    bool first_iteration_has_been_done_ = false;
 };
 
 }  // namespace eigendecompositor
