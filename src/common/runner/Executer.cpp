8551436e3ba8d05857eb1bee2871a9f8c07348db
Eugen Kadienko <emkad@yandex.ru>
2024-04-02 18:29:12 +0700

    Refactor Executer.cpp.

--- src/common/runner/Executer.cpp
+++ src/common/runner/Executer.cpp
@@ -1,5 +1,7 @@
 #include "Executer.h"
 
+#include "src/common/Logger.h"
+
 namespace runner {
 void Executer::execute(input::Parser parser) {
     auto optimization_list = parser.getOptimizationList().value();
@@ -9,22 +11,21 @@ void Executer::execute(input::Parser parser) {
 
         if (parser.getTemperaturesForSimulation().has_value()) {
             for (auto temp : parser.getTemperaturesForSimulation().value()) {
-                std::cout << temp << " " <<
-                    runner.getMagneticSusceptibilityController().calculateTheoreticalMuSquared(temp)
-                          << std::endl;
+                double value = runner.getMagneticSusceptibilityController().calculateTheoreticalMuSquared(temp);
+                common::Logger::basic("{:.8}    {:.8}", temp, value);
             }
         } else if (parser.getNonlinearSolver().has_value()) {
             runner.initializeExperimentalValues(parser.getExperimentalValuesWorker().value());
 
             runner.minimizeResidualError(parser.getNonlinearSolver().value());
 
+            common::Logger::basic_msg("\nMuSquared, Temperature");
             auto theor_final = runner.getMagneticSusceptibilityController().getTheoreticalValues();
-
-            std::cout << std::endl;
             for (auto [temperature, value] : theor_final) {
-                std::cout << temperature << " " << value << std::endl;
+                common::Logger::basic("{:.8e}    {:.8e}", temperature, value);
             }
         }
     }
+    common::Logger::separate(0, common::basic);
 }
 }  // namespace runner
