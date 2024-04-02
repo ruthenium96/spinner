bba5c5707f76fcbe01c5f32e381116ee24a12d1c
Eugen Kadienko <emkad@yandex.ru>
2024-04-02 18:25:46 +0700

    Refactor src/main.cpp and tests/main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 
+#include "src/common/Logger.h"
 #include "src/common/runner/Executer.h"
 #include "src/input/Parser.h"
 
 int main(int argc, char *argv[]) {
+    common::Logger::set_pattern("[%C/%m/%d %H:%M:%S.%e] %v");
     if (argc == 1) {
-        std::cout << "ERROR: missing input file" << std::endl;
-        return 0;
+        common::Logger::error_msg("Missing input file.");
+        return -1;
     }
     if (argc > 2) {
-        std::cout << "ERROR: Too much arguments" << std::endl;
-        return 0;
+        common::Logger::error_msg("Too much arguments.");
+        return -1;
     }
-    auto parser = input::Parser(argv[1]);
-    runner::Executer::execute(parser);
+    common::Logger::basic_msg("The calculations have started");
+    common::Logger::separate(0, common::basic);
+    try {
+        auto parser = input::Parser(argv[1]);
+        runner::Executer::execute(parser);
+    } catch (const std::exception& e) {
+        common::Logger::error_msg("An error occurred during program execution:\n{}", e.what());
+        return -1;
+    }
+    common::Logger::basic_msg("The calculations have been completed successfully.");
     return 0;
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 
+#include "src/common/Logger.h"
+
 int main(int argc, char** argv) {
+    common::Logger::set_level(common::PrintLevel::off);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
