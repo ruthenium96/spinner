bcccd5a8e12f7b1a58180f43d13d073d984c45f9
Eugen Kadienko <emkad@yandex.ru>
2025-02-26 15:35:38 +0700

    Add logging to WorkerConstructor.

--- src/entities/magnetic_susceptibility/worker/WorkerConstructor.cpp
+++ src/entities/magnetic_susceptibility/worker/WorkerConstructor.cpp
@@ -1,5 +1,7 @@
 #include "WorkerConstructor.h"
 
+#include "src/common/Logger.h"
+#include "src/common/Quantity.h"
 #include "src/entities/magnetic_susceptibility/worker/CurieWeissWorker.h"
 #include "src/entities/magnetic_susceptibility/worker/GSzSquaredWorker.h"
 #include "src/entities/magnetic_susceptibility/worker/UniqueGOnlySSquaredWorker.h"
@@ -10,6 +12,9 @@ std::unique_ptr<AbstractWorker> WorkerConstructor::construct(
     const model::Model& model,
     const std::unique_ptr<eigendecompositor::AbstractEigendecompositor>& eigendecompositor,
     const quantum::linear_algebra::FactoriesList& factories) {
+
+    common::Logger::detailed_msg("magnetic_susceptibility::Worker information:");
+    
     auto energy_vector = factories.createVector();
     auto degeneracy_vector = factories.createVector();
 
@@ -36,6 +41,7 @@ std::unique_ptr<AbstractWorker> WorkerConstructor::construct(
             s_squared_vector->concatenate_with(subspectrum.raw_data);
         }
 
+        common::Logger::detailed("UniqueGOnlySSquaredWorker will be used.");
         magnetic_susceptibility_worker =
             std::make_unique<magnetic_susceptibility::worker::UniqueGOnlySSquaredWorker>(
                 std::move(energy_vector),
@@ -49,6 +55,7 @@ std::unique_ptr<AbstractWorker> WorkerConstructor::construct(
             g_sz_squared_vector->concatenate_with(subspectrum.raw_data);
         }
 
+        common::Logger::detailed("GSzSquaredWorker will be used.");
         magnetic_susceptibility_worker =
             std::make_unique<magnetic_susceptibility::worker::GSzSquaredWorker>(
                 std::move(energy_vector),
@@ -59,11 +66,14 @@ std::unique_ptr<AbstractWorker> WorkerConstructor::construct(
     }
 
     if (symbolic_worker.isThetaInitialized()) {
+        common::Logger::detailed("CurieWeissWorker will be used.");
         magnetic_susceptibility_worker =
             std::make_unique<magnetic_susceptibility::worker::CurieWeissWorker>(
                 std::move(magnetic_susceptibility_worker),
                 model.getNumericalWorker().getThetaParameter());
     }
+
+    common::Logger::separate(0, common::detailed);
 
     return magnetic_susceptibility_worker;
 
