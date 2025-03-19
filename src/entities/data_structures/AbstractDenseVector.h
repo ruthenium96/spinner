b97ad97022272da9bb8be930bbd60132fcb92f7a
Eugen Kadienko <emkad@yandex.ru>
2025-03-19 21:42:59 +0700

    Another fix of includes.

--- src/entities/data_structures/AbstractDenseVector.h
+++ src/entities/data_structures/AbstractDenseVector.h
@@ -3,7 +3,6 @@
 
 #include <cstdint>
 #include <memory>
-#include <vector>
 
 namespace quantum::linear_algebra {
 class AbstractDenseVector {
--- tests/tools/AllSparseSemiunitaryMatrixFactories.h
+++ tests/tools/AllSparseSemiunitaryMatrixFactories.h
@@ -1,9 +1,10 @@
 #ifndef SPINNER_ALLSPARSESEMIUNITARYMATRIXFACTORIES_H
 #define SPINNER_ALLSPARSESEMIUNITARYMATRIXFACTORIES_H
 
-#include "src/entities/data_structures/arma/ArmaFactories.h"
-#include "src/entities/data_structures/eigen/EigenFactories.h"
-#include "src/entities/data_structures/hashmap/HashmapFactories.h"
+#include <memory>
+#include <vector>
+
+#include "src/entities/data_structures/AbstractFactories.h"
 
 std::vector<std::shared_ptr<quantum::linear_algebra::AbstractSparseTransformFactory>>
 constructAllSparseSemiunitaryMatrixFactories();
