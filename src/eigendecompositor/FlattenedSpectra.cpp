55a6c96cb408d4faeff28dd6528d27c9c24f7c5e
Eugen Kadienko <emkad@yandex.ru>
2025-03-20 16:03:42 +0700

    Implement flattened spectrum derivatives.

--- src/eigendecompositor/FlattenedSpectra.cpp
+++ src/eigendecompositor/FlattenedSpectra.cpp
@@ -29,6 +29,23 @@ void FlattenedSpectra::updateValues(const AllQuantitiesGetter& allQuantitiesGett
     flattenedSpectra_[common::Energy]->subtract_minimum();
 }
 
+void FlattenedSpectra::updateDerivativeValues(const AllQuantitiesGetter& allQuantitiesGetter,
+    const std::vector<model::symbols::SymbolName>& symbol_names,
+    const quantum::linear_algebra::FactoriesList& factories) {
+    flattenedDerivativeSpectra_.clear();
+    for (const auto& quantity_enum : magic_enum::enum_values<common::QuantityEnum>()) {
+        for (const auto& symbol_name : symbol_names) {
+            auto maybe_spectrum = allQuantitiesGetter.getSpectrumDerivative(quantity_enum, symbol_name);
+            if (maybe_spectrum.has_value()) {
+                flattenedDerivativeSpectra_[{quantity_enum, symbol_name}] = factories.createVector();
+                for (const auto& subspectrum : maybe_spectrum.value().get().blocks) {
+                    flattenedDerivativeSpectra_[{quantity_enum, symbol_name}]->concatenate_with(subspectrum.raw_data);
+                }
+            }    
+        }
+    }   
+}
+
 std::optional<std::reference_wrapper<const std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>>> 
     FlattenedSpectra::getFlattenSpectrum(common::QuantityEnum quantity_enum) const {
     if (flattenedSpectra_.contains(quantity_enum)) {
@@ -37,6 +54,17 @@ std::optional<std::reference_wrapper<const std::unique_ptr<quantum::linear_algeb
         return std::nullopt;
     }
 }
+
+std::optional<std::reference_wrapper<const std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>>> 
+    FlattenedSpectra::getFlattenDerivativeSpectrum(common::QuantityEnum quantity_enum, 
+        const model::symbols::SymbolName& symbol_name) const {
+    if (flattenedDerivativeSpectra_.contains({quantity_enum, symbol_name})) {
+        return flattenedDerivativeSpectra_.at({quantity_enum, symbol_name});
+    } else {
+        return std::nullopt;
+    }
+}
+
 
 const std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>& 
     FlattenedSpectra::getDegeneracyValues() const {
--- src/eigendecompositor/FlattenedSpectra.h
+++ src/eigendecompositor/FlattenedSpectra.h
@@ -8,6 +8,7 @@
 #include "src/common/Quantity.h"
 #include "src/eigendecompositor/AllQuantitiesGetter.h"
 #include "src/entities/data_structures/AbstractDenseVector.h"
+#include "src/model/symbols/SymbolName.h"
 
 namespace eigendecompositor {
 class FlattenedSpectra {
@@ -15,13 +16,22 @@ class FlattenedSpectra {
     FlattenedSpectra() = default;
     void updateValues(const AllQuantitiesGetter& allQuantitiesGetter,
         const quantum::linear_algebra::FactoriesList& factories);
+    void updateDerivativeValues(const AllQuantitiesGetter& allQuantitiesGetter,
+        const std::vector<model::symbols::SymbolName>& symbol_names,
+        const quantum::linear_algebra::FactoriesList& factories);
+  
     std::optional<std::reference_wrapper<const std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>>> 
         getFlattenSpectrum(common::QuantityEnum quantity_enum) const;
+    std::optional<std::reference_wrapper<const std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>>> 
+        getFlattenDerivativeSpectrum(common::QuantityEnum quantity_enum, 
+          const model::symbols::SymbolName& symbol_name) const;
     const std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>& getDegeneracyValues() const;
 
   private:
     std::map<common::QuantityEnum, 
         std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>> flattenedSpectra_;
+    std::map<std::pair<common::QuantityEnum, model::symbols::SymbolName>, 
+        std::unique_ptr<quantum::linear_algebra::AbstractDenseVector>> flattenedDerivativeSpectra_;
     std::unique_ptr<quantum::linear_algebra::AbstractDenseVector> degeneracyValues_;
 };
 } // namespace eigendecompositor
